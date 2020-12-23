b010b49904f202def0f11d67df07530214256c11
jjcetraro <jjcetraro@gmail.com>
2020-12-23 19:09:31 -0300

    jjcetraro

--- app/src/main/java/com/example/ffmpegsrt/FirstFragment.java
+++ app/src/main/java/com/example/ffmpegsrt/FirstFragment.java
@@ -6,14 +6,26 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.arthenica.mobileffmpeg.Config;
+import com.arthenica.mobileffmpeg.FFmpeg;
+
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.Scanner;
+
+import static com.arthenica.mobileffmpeg.Config.RETURN_CODE_CANCEL;
+import static com.arthenica.mobileffmpeg.Config.RETURN_CODE_SUCCESS;
 
 public class FirstFragment extends Fragment {
 
@@ -32,14 +44,47 @@ public class FirstFragment extends Fragment {
         view.findViewById(R.id.button_first).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                FFmpegHelper.saveVideo(getActivity());
+                /*FFmpegHelper.saveVideo(getActivity());
                 Thread t = new Thread(new Runnable() {
                     public void run() {
                         getVideo();
                     }
-                });
+                });*/
+
+                startListening();
+
+                /*new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try{
+                            Socket socket = new Socket("0.0.0.0", 7800);
+                            PrintWriter printWriter = new PrintWriter(socket.getOutputStream());
+                            printWriter.write("holaaa");
+                            printWriter.flush();
+                            printWriter.close();
+                            socket.close();
+                            Log.d("dddddddd", "message sent!");
+                        }catch (Exception ex){
+                            StringWriter errors = new StringWriter();
+                            ex.printStackTrace(new PrintWriter(errors));
+                            Log.d("dddddddd", errors.toString());
+                        }
+                    }
+                }).start();*/
 
                 //t.start();
+
+
+                int rc = FFmpeg.execute("-f android_camera -video_size hd720 -input_queue_size 60 -i 0:0 -r 30 -f h264 udp://0.0.0.0:7800");
+
+                if (rc == RETURN_CODE_SUCCESS) {
+                    Log.d(Config.TAG, "Command execution completed successfully.");
+                } else if (rc == RETURN_CODE_CANCEL) {
+                    Log.d(Config.TAG, "Command execution cancelled by user.");
+                } else {
+                    Log.d(Config.TAG, String.format("Command execution failed with rc=%d and the output below.", rc));
+                    Config.printLastCommandOutput(Log.INFO);
+                }
             }
         });
     }
@@ -57,5 +102,42 @@ public class FirstFragment extends Fragment {
         }catch (Exception e) {
             Log.e(String.valueOf(e), "");
         }
+    }
+
+    private void startListening(){
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                /*try{
+                    ServerSocket serverSocket = new ServerSocket(7800);
+                    while(true){
+                        Socket socket = serverSocket.accept();
+                        InputStreamReader inputStreamReader = new InputStreamReader(socket.getInputStream());
+                        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                        String message = bufferedReader.readLine();
+                        Log.d("dddddddd", message);
+                    }
+                }catch (Exception ex){
+                    StringWriter errors = new StringWriter();
+                    ex.printStackTrace(new PrintWriter(errors));
+                    Log.d("dddddddd", errors.toString());
+                }*/
+
+                try{
+                    byte[] buffer = new byte[512];
+                    DatagramSocket socketUDP = new DatagramSocket(7800);
+                    while(true){
+                        DatagramPacket peticion = new DatagramPacket(buffer, buffer.length);
+                        socketUDP.receive(peticion);
+                        String message = new String(peticion.getData());
+                        Log.d("dddddddd", message);
+                    }
+                }catch (Exception ex){
+                    StringWriter errors = new StringWriter();
+                    ex.printStackTrace(new PrintWriter(errors));
+                    Log.d("dddddddd", errors.toString());
+                }
+            }
+        }).start();
     }
 }
---
+++ app/src/main/java/com/example/ffmpegsrt/MainActivity2.java
@@ -0,0 +1,136 @@
+package com.example.ffmpegsrt;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.util.Pair;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.arthenica.mobileffmpeg.Config;
+import com.arthenica.mobileffmpeg.FFmpeg;
+import com.github.thibaultbee.srtdroid.Srt;
+import com.github.thibaultbee.srtdroid.enums.SockOpt;
+import com.github.thibaultbee.srtdroid.models.Socket;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetSocketAddress;
+
+import static com.arthenica.mobileffmpeg.Config.RETURN_CODE_CANCEL;
+import static com.arthenica.mobileffmpeg.Config.RETURN_CODE_SUCCESS;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    // views
+    private Button mButtonSendSrtMessage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        // init
+        Srt srt = new Srt();
+        srt.startUp();
+
+        // views binding
+        mButtonSendSrtMessage = findViewById(R.id.button_send_srt_message);
+        mButtonSendSrtMessage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startSendingSRTStream();
+            }
+        });
+    }
+
+    //---------------------------------------------------------------------------------------------
+    //---------------------------------------------------------------------------------------------
+    //-------------------------------------- PRIVATE METHODS --------------------------------------
+    //---------------------------------------------------------------------------------------------
+    //---------------------------------------------------------------------------------------------
+
+    private void startSendingSRTStream(){
+        // create SRT sockeet to send stream
+        Socket clientSocket = new Socket();
+        if(!clientSocket.isValid()){
+            Log.d("dddddddddd", "clientSocket is not valid");
+            return;
+        }
+        //clientSocket.setSockFlag(SockOpt.RCVSYN, true);
+        //clientSocket.connect("192.168.43.50", 11001);
+        clientSocket.connect("192.168.43.50", 9001);
+        Log.d("dddddddddd", "client socket connected!");
+
+        // create udp listening on 0.0.0.0:7800
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try{
+                    byte[] buffer = new byte[512];
+                    DatagramSocket socketUDP = new DatagramSocket(7800);
+                    while(true){
+                        DatagramPacket peticion = new DatagramPacket(buffer, buffer.length);
+                        socketUDP.receive(peticion);
+                        byte[] data = peticion.getData();
+                        int ret = clientSocket.send(data);
+                        //Log.d("dddddddd", "ret: " + ret);
+                        String message = data[data.length-1] + "";
+                        Log.d("dddddddd", message);
+                    }
+                }catch (Exception ex){
+                    StringWriter errors = new StringWriter();
+                    ex.printStackTrace(new PrintWriter(errors));
+                    Log.d("dddddddd", errors.toString());
+                }
+            }
+        }).start();
+
+
+
+
+
+        // start sending camera stream to 0.0.0.0:7800 by udp
+        int rc = FFmpeg.execute("-f android_camera -video_size hd720 -input_queue_size 60 -i 0:0 -r 30 -f h264 udp://0.0.0.0:7800");
+
+        if (rc == RETURN_CODE_SUCCESS) {
+            Log.d(Config.TAG, "Command execution completed successfully.");
+        } else if (rc == RETURN_CODE_CANCEL) {
+            Log.d(Config.TAG, "Command execution cancelled by user.");
+        } else {
+            Log.d(Config.TAG, String.format("Command execution failed with rc=%d and the output below.", rc));
+            Config.printLastCommandOutput(Log.INFO);
+        }
+    }
+
+    /*private void startSrtServer(){
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Socket socketServer = new Socket();
+                socketServer.setSockFlag(SockOpt.RCVSYN, true);
+                socketServer.bind("0.0.0.0", 50001);
+                socketServer.listen(1);
+                Log.d("dddddddddd", "before accept");
+                Pair<Socket, InetSocketAddress> pair = socketServer.accept();
+                Socket clienteSocket = pair.first;
+                //cie
+                Log.d("dddddddddd", "after accept");
+                //showToast("SRT Server Listening!");
+            }
+        }).start();
+    }*/
+
+    private void showToast(String message){
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(MainActivity2.this, message, Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
